e61d4962be309fe1ecfa469cbd8aaa73813e43a3
wWX391768 <wangchaoyang@huawei.com>
2017-04-22 17:50:57 +0800

    image load

--- libOC/Category/UIImage/UIImage+Ext.h
+++ libOC/Category/UIImage/UIImage+Ext.h
@@ -10,6 +10,10 @@
 
 @interface UIImage (Ext)
 
+#define UIImageLINK(v) #v
+#define UIImageGET(v) [UIImage imageWithPath:UIImageLINK(v)]
++ (UIImage *)imageWithPath:(const char *)path;
+
 + (UIImage *)resizableImageWithImageName:(NSString *)imageName;
 
 @end
--- libOC/Category/UIImage/UIImage+Ext.m
+++ libOC/Category/UIImage/UIImage+Ext.m
@@ -10,6 +10,69 @@
 
 @implementation UIImage (Ext)
 
++ (UIImage *)imageWithPath:(const char *)path
+{
+    if (NULL == path) {
+        return nil;
+    }
+    if (0 == strlen(path)) {
+        return nil;
+    }
+    NSString *const ocPath = [NSString stringWithUTF8String:path];
+    if ([ocPath hasPrefix:@"/"]) {
+        return [[UIImage alloc] initWithContentsOfFile:ocPath];
+    }
+    const NSInteger pathLen = ocPath.length;
+    NSString *fileTail = nil;
+    NSString *realPath = nil;
+    if (3 < pathLen)
+    {
+        fileTail = [[ocPath substringFromIndex:pathLen - 4] lowercaseString];
+        if ([fileTail isEqual:@".png"] || [fileTail isEqual:@".jpg"] || [fileTail isEqual:@".gif"])
+        {
+            realPath = ocPath;
+        }
+        else if ([fileTail isEqual:@"jpeg"] || [fileTail isEqual:@"tiff"])
+        {
+            realPath = ocPath;
+        }
+    }
+    NSBundle *const bundle = [NSBundle mainBundle];
+    if (nil != realPath)
+    {
+        NSString *imgPath = [bundle pathForResource:[ocPath substringToIndex:pathLen - 4] ofType:[ocPath substringFromIndex:pathLen - 4]];
+        return [[UIImage alloc] initWithContentsOfFile:imgPath];
+    }
+    NSString *screenScale = [NSString stringWithFormat:@"%1.f", [[UIScreen mainScreen] scale]];
+    realPath = [NSString stringWithFormat:@"%@@%@x", ocPath, screenScale];
+    NSArray<NSString *> *const fileExts = @[@"png", @"PNG", @"jpg", @"JPG", @"gif", @"GIF", @"jpeg", @"JPEG", @"tiff", @"TIFF"];
+    for (NSInteger i = 0, cnt = fileExts.count; i < cnt; i++)
+    {
+        NSString *imgPath = [bundle pathForResource:realPath ofType:[fileExts objectAtIndex:i]];
+        if (nil != imgPath) {
+            return [[UIImage alloc] initWithContentsOfFile:imgPath];
+        }
+    }
+    screenScale = [screenScale isEqual:@"2"] ? @"3" : @"2";
+    realPath = [NSString stringWithFormat:@"%@@%@x", ocPath, screenScale];
+    for (NSInteger i = 0, cnt = fileExts.count; i < cnt; i++)
+    {
+        NSString *imgPath = [bundle pathForResource:realPath ofType:[fileExts objectAtIndex:i]];
+        if (nil != imgPath) {
+            return [[UIImage alloc] initWithContentsOfFile:imgPath];
+        }
+    }
+    realPath = ocPath;
+    for (NSInteger i = 0, cnt = fileExts.count; i < cnt; i++)
+    {
+        NSString *imgPath = [bundle pathForResource:realPath ofType:[fileExts objectAtIndex:i]];
+        if (nil != imgPath) {
+            return [[UIImage alloc] initWithContentsOfFile:imgPath];
+        }
+    }
+    return [[UIImage alloc] initWithContentsOfFile:[bundle pathForResource:ocPath ofType:nil]];
+}
+
 + (UIImage *)resizableImageWithImageName:(NSString *)imageName
 {
     UIImage *im = [UIImage imageNamed:imageName];
